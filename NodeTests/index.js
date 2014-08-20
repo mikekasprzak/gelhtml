5a7c23338f59ad8b440326f0fa0ffc60f72459f8
mike <mike@localhost>
2014-08-20 02:59:50 -0400

    Added some initial Node support (and tests)

---
+++ NodeTests/index.js
@@ -0,0 +1,26 @@
+
+"use strict"
+
+var GelNode = require("../lib/gelNode.js");
+var GelMath = require("../lib/gelMath.js");
+
+console.log("I am", GelMath.hasGelMath);
+
+this.nope = 10;
+
+var song = true;
+global.ken = "HERR!";
+
+
+console.log(this);
+console.log(module);
+//console.log(global);
+
+console.log( global.song );
+console.log( global.shawn );
+
+console.log(!!global.hasGelMath);
+console.log(!!global.hasGelNode);
+console.log(!!global.hasGelNodeo);
+console.log(ken);
+console.log(global.ken);
